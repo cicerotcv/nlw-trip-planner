81b7f99d155dcc3b519ec80946688e18e0db957b
cicerotcv <elm.tiago@gmail.com>
2024-07-09 00:55:34 -0300

    feat: Update App component to use SafeAreaProvider and SplashScreen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import * as Splash from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Loading } from '@/components/loading';
+import { useFontsLoaded } from '@/hooks/fonts-loaded';
+import { NavigationRoutes } from '@/modules/navigation';
+import '@/styles/global.css';
+
+Splash.preventAutoHideAsync();
 
 export default function App() {
+  const isFontsLoaded = useFontsLoaded();
+
+  if (!isFontsLoaded) return <Loading />;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={{ flex: 1 }}>
+      <NavigationRoutes onReady={Splash.hideAsync} />
+      <StatusBar translucent style="light" backgroundColor="transparent" />
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
