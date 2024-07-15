8a908b2fab3d9424ae210b70c882252344d8af9b
cicerotcv <elm.tiago@gmail.com>
2024-07-15 01:15:13 -0300

    feat: Add QueryClientProvider to App component

--- App.tsx
+++ App.tsx
@@ -3,10 +3,15 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import * as Splash from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { Loading } from '@/components/loading';
 import { useFontsLoaded } from '@/hooks/fonts-loaded';
 import { NavigationRoutes } from '@/modules/navigation';
 import '@/styles/global.css';
+import '@/utils/daysjs-locale-config';
+
+const queryClient = new QueryClient();
 
 Splash.preventAutoHideAsync();
 
@@ -16,9 +21,11 @@ export default function App() {
   if (!isFontsLoaded) return <Loading />;
 
   return (
-    <SafeAreaProvider style={{ flex: 1 }}>
-      <NavigationRoutes onReady={Splash.hideAsync} />
-      <StatusBar translucent style="light" backgroundColor="transparent" />
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider style={{ flex: 1 }}>
+        <NavigationRoutes onReady={Splash.hideAsync} />
+        <StatusBar translucent style="light" backgroundColor="transparent" />
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
