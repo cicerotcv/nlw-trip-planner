4ae691ac9b59a1136aae0225b453bc9349d95bad
cicerotcv <elm.tiago@gmail.com>
2024-07-15 01:20:19 -0300

    feat: Add new trip, trip details, and trip list screens to planner module

--- src/modules/navigation/navigators/root-stack/index.tsx
+++ src/modules/navigation/navigators/root-stack/index.tsx
@@ -1,5 +1,8 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { NewTripScreen } from '@/modules/planner/screens/new-trip';
+import { TripDetailsScreen } from '@/modules/planner/screens/trip-details';
+import { TripListScreen } from '@/modules/planner/screens/trips-list';
 import { colors } from '@/styles/colors';
 
 import { RootStackParams } from './params';
@@ -18,7 +21,18 @@ export const RootStackNavigator = () => {
       }}
       initialRouteName={RootStackRoutes.TripList}
     >
-     
+      <RootStack.Screen
+        name={RootStackRoutes.NewTrip}
+        component={NewTripScreen}
+      />
+      <RootStack.Screen
+        name={RootStackRoutes.TripDetails}
+        component={TripDetailsScreen}
+      />
+      <RootStack.Screen
+        name={RootStackRoutes.TripList}
+        component={TripListScreen}
+      />
     </RootStack.Navigator>
   );
 };
