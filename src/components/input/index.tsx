c7eea0b21b15bee2b01ed178c6432ef53d3724a4
cicerotcv <elm.tiago@gmail.com>
2024-07-15 01:09:27 -0300

    feat: Refactor Input component to include error prop and improve styling

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,29 +1,50 @@
 import clsx from 'clsx';
 
-import { Platform, TextInput, TextInputProps, View } from 'react-native';
+import {
+  Platform,
+  Text,
+  TextInput,
+  TextInputProps,
+  View,
+  ViewProps,
+} from 'react-native';
 
 import { colors } from '@/styles/colors';
 
 type Variants = 'primary' | 'secondary' | 'tertiary';
 
-type InputProps = {
+type InputProps = ViewProps & {
   children?: React.ReactNode;
   variant?: Variants;
+  error?: string;
 };
 
-const Input = ({ variant = 'primary', children }: InputProps) => {
+const Input = ({
+  variant = 'primary',
+  error,
+  children,
+  className,
+  ...props
+}: InputProps) => {
   return (
-    <View
-      className={clsx('w-full h-16 flex-row items-center gap-2', {
-        'h-14 px-4 rounded-lg border border-zinc-800': variant !== 'primary',
-        'bg-zinc-950': variant === 'secondary',
-        'bg-zinc-900': variant === 'tertiary',
-      })}
-    >
-      {children}
+    <View className={clsx('flex-col gap-2', className)} {...props}>
+      <View
+        className={clsx('w-full h-12 flex-row items-center gap-2', {
+          'h-14 px-4 rounded-lg border border-zinc-800': variant !== 'primary',
+          'bg-zinc-950': variant === 'secondary',
+          'bg-zinc-900': variant === 'tertiary',
+        })}
+      >
+        {children}
+      </View>
+
+      {!!error && (
+        <Text className="font-semibold text-sm text-rose-500">{error}</Text>
+      )}
     </View>
   );
 };
+
 type FieldProps = TextInputProps & {};
 
 const Field: React.FC<FieldProps> = ({ className, ...props }) => {
